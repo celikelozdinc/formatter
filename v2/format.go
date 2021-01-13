70bb45abfc2b0190719bb3beabcf392304685930
celikelozdinc <ozdinc.celikel@siemens.com>
2021-01-13 16:39:25 +0300

    v2
    Signed-off-by: celikelozdinc <ozdinc.celikel@siemens.com>

---
+++ v2/format.go
@@ -0,0 +1,15 @@
+package formatter
+
+import "strings"
+
+const (
+	withGreen string = "\033[32m"
+	withRed   string = "\033[31m"
+	reset     string = "\033[0m"
+)
+
+// Format prints the input string in a proper format
+func Format(input string) string {
+	formatted := []string{withGreen, input, reset}
+	return strings.Join(formatted, "")
+}
---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/celikelozdinc/formatter/v2
+
+go 1.15
